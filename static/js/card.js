0c8a369666f42a737f4ffe380ea23747483a6337
Milton Luís <miltonluis138@gmail.com>
2022-12-29 15:56:22 -0300

    cards js

---
+++ static/js/card.js
@@ -0,0 +1,8 @@
+// document.location.reload(true)
+
+document.getElementById('Abjuração').style.cssText =
+    `background-image: radial-gradient(yellow, black)`;
+document.getElementById('Transmutação').style.backgroundImage = "radial-gradient(blue, black)";
+document.getElementById('Adivinhação').style.backgroundImage = "radial-gradient(violet, black)";
+document.getElementById('Evocação').style.backgroundImage = "radial-gradient(red, black)";
+
